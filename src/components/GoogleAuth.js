54083323551d3b319491f2c6cce93855725cb9ac
Chau ShunWai <swchau@connect.ust.hk>
2019-02-08 16:56:07 +0800

    whatever

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -7,7 +7,7 @@ class GoogleAuth extends React.Component {
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: '88520721261-32agcpmf7pf3oocj25b6qujkf4j5vnv4.apps.googleusercontent.com',
+                clientId: '88520721261-3bca3hi8tvb57il372rpfbk621ghi0dp.apps.googleusercontent.com',
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
