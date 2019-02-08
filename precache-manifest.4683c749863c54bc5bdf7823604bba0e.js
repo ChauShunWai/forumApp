a2692d1c77478a3b6485c87b3b66fe5c757a69d2
Chau ShunWai <swchau@connect.ust.hk>
2019-02-08 15:54:09 +0800

    Updates

---
+++ precache-manifest.4683c749863c54bc5bdf7823604bba0e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cc1c6ab0f481103e304",
+    "url": "/forumApp/static/js/runtime~main.4cc1c6ab.js"
+  },
+  {
+    "revision": "b0f83eebaed18293832c",
+    "url": "/forumApp/static/js/main.b0f83eeb.chunk.js"
+  },
+  {
+    "revision": "f9e842b0a855f3ad9273",
+    "url": "/forumApp/static/js/1.f9e842b0.chunk.js"
+  },
+  {
+    "revision": "5a04307c609ee37d72d222a5ca1480ee",
+    "url": "/forumApp/index.html"
+  }
+];
